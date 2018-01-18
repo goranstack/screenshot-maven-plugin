b7c895be712d736afbdd2f8bbfc6e3b2672992b2
Göran Stäck <goran.stack@atg.se>
2018-01-18 15:46:51 +0100

    Updated GitHub pages index.adoc with more examples

--- gh-pages/push-to-gh-pages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd asciidoc/target
-rm -rf site
-git init
-git remote add origin git@github.com:goranstack/screenshot-maven-plugin.git
-git add .
-git commit -m "Updated generated doc"
-git push --force origin master:gh-pages
-rm -rf .git 
