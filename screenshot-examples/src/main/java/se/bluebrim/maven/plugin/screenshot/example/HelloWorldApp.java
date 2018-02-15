fcf9d811edcd781bdfa62fcb73299185e4841ffb
Göran Stäck <goran.stack@atg.se>
2018-02-15 16:30:31 +0100

    Reorganized the documentation

--- screenshot-examples/src/main/java/se/bluebrim/maven/plugin/screenshot/example/HelloWorldApp.java
+++ screenshot-examples/src/main/java/se/bluebrim/maven/plugin/screenshot/example/HelloWorldApp.java
@@ -1,43 +1,44 @@
 package se.bluebrim.maven.plugin.screenshot.example;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 /**
- * Demonstrates the use of a JVM splash screen created by the Screenshot Maven Plugin.
- * Run mvn install to get a target/classes/splash.png image of the {@link SplashScreenPanel}
- * Specify the VM argument:
+ * Demonstrates the use of a JVM splash screen created by the Screenshot Maven
+ * Plugin. Run mvn install to get a target/classes/splash.png image of the
+ * {@link SplashScreenPanel} Specify the VM argument:
  * -splash:target/classes/splash.png
- *  
+ * 
  * @author Goran Stack
  *
  */
 public class HelloWorldApp {
-	
+
 	public static void main(String[] args) throws InterruptedException {
-		SwingUtilities.invokeLater(() -> new HelloWorldApp().openInWindow());
+		new HelloWorldApp().openInWindow();
 	}
-	
-	private void openInWindow()
-	{
+
+	private void openInWindow() {
 		JFrame window = new JFrame(getClass().getSimpleName());
 		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		window.getContentPane().add(createWindowContent());
 		window.pack();
 		window.setLocationRelativeTo(null);
-		try {
-			Thread.sleep(4000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-		window.setVisible(true);		
-	}	
+		new Timer().schedule(new TimerTask() {
 
+			@Override
+			public void run() {
+				SwingUtilities.invokeLater(() -> window.setVisible(true));
+			}
+		}, 3000);
+	}
 
-	public JComponent createWindowContent()
-	{
+	public JComponent createWindowContent() {
 		return new HelloWorldPanel();
 	}
 }
