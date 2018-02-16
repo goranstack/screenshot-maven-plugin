80b2ad1e2bb18af8f4e96c8aa6695790d4fa4d9f
Göran Stäck <goran.stack@atg.se>
2018-02-16 14:50:20 +0100

    Updated the documentation

--- screenshot-examples/src/test/java/se/bluebrim/maven/plugin/screenshot/example/HelloWorldPanelTest.java
+++ screenshot-examples/src/test/java/se/bluebrim/maven/plugin/screenshot/example/HelloWorldPanelTest.java
@@ -2,31 +2,10 @@ package se.bluebrim.maven.plugin.screenshot.example;
 
 import javax.swing.JComponent;
 
-import org.jdesktop.swingx.JXFrame;
-
 import se.bluebrim.maven.plugin.screenshot.Screenshot;
 
-/**
- * 
- * @author Goran Stack
- *
- */
 public class HelloWorldPanelTest {
 	
-	public static void main(String[] args) {
-		new HelloWorldPanelTest().openInWindow();
-	}
-	
-	private void openInWindow()
-	{
-		JXFrame window = new JXFrame(getClass().getSimpleName(), true);
-		window.getContentPane().add(createScreenShot());
-		window.pack();
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);		
-	}	
-
-
 	@Screenshot
 	public JComponent createScreenShot()
 	{
