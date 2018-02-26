4fc5cd7a91063498f7995a5620da88311afd3b1a
Göran Stäck <goran.stack@atg.se>
2018-02-26 10:22:33 +0100

    Reorganized project to fix missing parent pom deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,7 +8,6 @@ then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG
 
-	cd maven-central-parent
 
     if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
         shred -v ~/.gnupg/*
